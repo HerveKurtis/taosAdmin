ef5e897581becdc39b9c18d928e027c7ad08d597
Hervé Tendayi <tendayiherve@gmail.com>
2026-06-14 17:49:02 +0200

    feat(firebase): public config + load firebase.js module from index.html

---
+++ src/AdminTaos/wwwroot/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBwJSAxqrC9DjsNUECOGFjnDNT3YPp9snM",
+  authDomain: "taos-8d5f1.firebaseapp.com",
+  projectId: "taos-8d5f1",
+  storageBucket: "taos-8d5f1.firebasestorage.app",
+  messagingSenderId: "900636537680",
+  appId: "1:900636537680:web:c383cd9442007a87603255",
+  measurementId: "G-813RJ5HGP4"
+};
